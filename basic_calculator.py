3d16fedd8d3b46815a5b391f10a509f0bef58d87
Dynamic747 <jai_96rm@hotmail.co.uk>
2023-01-18 01:10:59 +0000

    basic_calculator

---
+++ basic_calculator.py
@@ -0,0 +1,3 @@
+first_num = input('Enter first number ')
+second_num = input('Enter Second Number ')
+print(int(first_num)+int(second_num))
--- test_file.py
+++ test_file.py
@@ -1,5 +1,3 @@
-import matplotlib.pyplot as plt
-import numpy as np 
 
 x = np.linspace(0,20,100)
 plt.plot(x, np.sin(x))
